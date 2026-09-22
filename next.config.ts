5bdfe25c789fdc58bb4eebfe4038aa410604f507
Dmitrijs Dums <dd636856@gmail.com>
2026-09-22 22:58:30 +0100

    feat: add GitHub Actions workflow for deploying Next.js to GitHub Pages and update next.config.ts for static export

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
-// next.config.js
-const nextConfig = {
+const nextConfig: NextConfig = {
   devIndicators: false,
-}
-
+  output: "export", // Enables static HTML export
+  basePath: "/my_profile", // Must match your GitHub repository name
+  assetPrefix: "/my_profile/", // Ensures static assets load correctly
+  images: {
+    unoptimized: true, // Required for next/image when doing a static export
+  },
+};
 
 export default nextConfig;
